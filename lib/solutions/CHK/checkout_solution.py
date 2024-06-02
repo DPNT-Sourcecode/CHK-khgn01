516523dbd5eb05f1d6c7fe8992c1d3348cdb78c3
sbx_user1051 <sbx_user1051@169.254.24.241>
2024-06-02 16:52:08 +0000

    Sun Jun 02 16:52:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ def checkout(skus):
 
     # create prices and offers
     price_table = {'A': 50, 'B':30, 'C':20, 'D': 15, 'E': 40}
-    offers = {'A':[(3,130), (5,200)], 'B':(2,45)}
+    multi_offers = {'A':[(3,130), (5,200)], 'B':(2,45)}
     free_offers = {'E':(2, 'B')}
 
     for char in skus:
@@ -18,16 +18,20 @@ def checkout(skus):
 
     total_price = 0
 
-    for item, (offer_count, offer_price) in offers.items():
-        if counts[item] >= offer_count:
-            total_price += (counts[item] // offer_count) * offer_price
-            counts[item] %= offer_count 
+    # do free offers first
+    for item, (required_qty, free_item) in free_offers.items():
+        if counts[item] >= required_qty:
+            free_qty = (counts[item] // required_qty)
+            counts[free_item] = max(0, counts[free_item] - free_qty)
+
+    # apply multi offers
+    for item, offers in multi_offers.items():
+        for offer_count, offer_price in sorted(offers, key=lambda x: -x[1]/x[0]):
+            if counts[item] >= offer_count:
+                total_price += (counts[item] // offer_count) * offer_price
+                counts[item] %= offer_count 
 
     for item, count in counts.items():
         total_price += count * price_table[item]
     
     return total_price
-
-
-
-
